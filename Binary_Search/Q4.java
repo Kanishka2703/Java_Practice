c175f309e7fab27d21666bb3763cb5f0d9998b68
Kanishka2703 <kanishkaasinghhh27@gmail.com>
2023-11-12 23:08:44 +0530

    QUES4

---
+++ Binary_Search/Q4.java
@@ -0,0 +1,22 @@
+//to implement the upper bound with brute force
+import java.util.*;
+public class Q4{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int arr[]={3,5,8,15,19};
+        int n=arr.length;
+        int flag=0;
+        System.out.println("Enter the number ");
+        int target=in.nextInt();
+        for(int i=0;i<n;i++){
+            if(arr[i]>target){
+                flag=1;
+                System.out.println("Upper bound is of index "+i);
+                break;
+            }
+        }
+        if(flag==0)
+        System.out.println("Upper bound is "+n);
+    }
+}
+
