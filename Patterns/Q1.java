592679d636dec2ca946633cea675dd947c4ec8c5
Kanishka2703 <kanishkaasinghhh27@gmail.com>
2024-03-20 12:47:31 +0530

    QUES1

---
+++ Patterns/Q1.java
@@ -0,0 +1,13 @@
+package Java_Practice.Patterns;
+import java.util.*;
+public class Q1{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
