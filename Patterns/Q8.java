ce164ab9828307f48a568206139863d5a8696f36
Kanishka2703 <kanishkaasinghhh27@gmail.com>
2024-04-07 22:40:23 +0530

    QUES8

---
+++ Patterns/Q8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Q8{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<n*2-(2*i+1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
