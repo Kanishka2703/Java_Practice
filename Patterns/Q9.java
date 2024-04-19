a01e595b0a4f02c74265ea01954e4dc9605278cc
Kanishka2703 <kanishkaasinghhh27@gmail.com>
2024-04-20 01:24:44 +0530

    QUES9

---
+++ Patterns/Q9.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Q9{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<i*2+1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<n*2-(2*i+1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
